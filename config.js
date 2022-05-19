450d00755ac31f74b67b635f82093c910249324e
Sai Main <saimain.venuslab@gmail.com>
2022-05-20 01:25:23 +0630

    init

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const TelegramToken = "5304902186:AAGHC1aY1b5HZrJaxRVQhWvgXGyQsEanRdI";
+const TelegramToken = "5316659013:AAHLwosc-87ajozG-62Dd-4vXEAWEINRUEY";
 const AdminId = "846655442";
 const StripeSecretKey =
   "sk_test_51J1h3xDwRgMLu02uqNf3IdfZxiSuBzcKc9zdKFdVuGgnZuKPbIwZNLJlIf9H9wNM0inFwYgbxEdRZV1bBwFXrBuh00lBiROLdp";
